f4020d110baa6716fef1429086edea6aa1694911
lunaissac <31965093+lunaissac@users.noreply.github.com>
2018-07-24 13:07:34 -0500

    Entregables del hackathon
    Entregables del hackathon

---
+++ Vistas.sql
@@ -0,0 +1,18 @@
+create view ListarRuta
+AS
+select * from Ruta
+GO
+create view ListarAsiento
+AS
+select * from Asiento
+GO
+create view ListarPasajero
+AS
+select * from Pasajero
+GO
+create view ListarSucursal
+AS
+select * from Sucursal
+GO
+
+select * from 
