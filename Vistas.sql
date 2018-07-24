a3614f8aa9645750eef47edd9e7a82135f585a54
lunaissac <31965093+lunaissac@users.noreply.github.com>
2018-07-24 14:01:56 -0500

    Update Vistas.sql

--- Vistas.sql
+++ Vistas.sql
@@ -14,5 +14,13 @@ create view ListarSucursal
 AS
 select * from Sucursal
 GO
-
-select * from 
+create view ListarEmpresa
+AS
+select  idEmpresa,razonSocial,direccion,teléfono,celular,email,ruc,Sucursal.lugar as Sucursal from Empresa
+inner join Sucursal on Sucursal.idSucursal = Empresa.Sucursal_idSucursal
+GO
+create view Listarchofer
+AS
+select idChofer,nombre,apellido,dni,celular,Bus.placa as Bus from  chofer
+inner join Bus on Bus.idBus = Chofer.Bus_idBus
+GO
